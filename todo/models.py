51f130c797480186fd752fd283bfeff1d2798b57
Roman Khabirov <khabirov_rr@bk.ru>
2022-10-16 20:57:05 +0300

    Добавил Todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(
+        verbose_name='Название задачи',
+        max_length=255
+    )
+    is_complete = models.BooleanField(
+        verbose_name='Завершено',
+        default=False
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
+        db_table = 'todos'
