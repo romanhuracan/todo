0e856115933df7fa4e898623981d1400068e5bdd
Roman Khabirov <khabirov_rr@bk.ru>
2022-10-16 23:40:50 +0300

    Добавил update статуса задания
    В urls.py прописал маршрут
    В views.py прописал логику обновления статуса задания
    В index.html прописал ссылки, по которым меняется статус задания

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('update/<int:todo_id>/', views.update_todo_is_complete, name='update_todo_is_complete')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from todo.models import Todo
 
@@ -6,3 +6,11 @@ from todo.models import Todo
 def index(request):
     todos = Todo.objects.all()
     return render(request, template_name='todo/index.html', context={'todos': todos})
+
+
+def update_todo_is_complete(request, todo_id):
+    """ Если статус задания True, то меняет его на False и наоборот. """
+    todo = Todo.objects.get(id=todo_id)
+    todo.is_complete = not todo.is_complete
+    todo.save()
+    return redirect('index')
